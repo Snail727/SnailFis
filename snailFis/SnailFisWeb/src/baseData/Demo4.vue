a356572259463a88a2a98680d929d3be380ea681
Snail727 <57943489+Snail727@users.noreply.github.com>
2020-05-18 23:38:11 +0800

    列表渲染

--- snailFis/SnailFisWeb/src/baseData/Demo4.vue
+++ snailFis/SnailFisWeb/src/baseData/Demo4.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
       <ol><li v-for="(model,index) in liList" :key="model.name">{{index}}---{{model.name}}</li></ol>
-      <!-- <button @click="liList.push({name:'d'})"><button> -->
-      <ol><li v-for="(model,name,index) in userObj" :key="model.name">{{index}}---{{name}}---{{model}}</li></ol>
+      <button @click="pushData">添加数据</button>
+      <ol><li v-for="(model,name,index) of userObj" :key="model.name">{{index}}---{{name}}---{{model}}</li></ol>
+      <ol><li v-for="n in 10" :key="n">{{n}}</li></ol>
+      <todo-item v-for="n of 9" :key="n"></todo-item>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+Vue.component('todo-item',{template:'<div>你好</div>'})
 export default {
   data(){
     return{
@@ -17,6 +21,11 @@ export default {
             note:"强壮"
         }
     }
+  },
+  methods:{
+    pushData(){
+      this.liList.push({name:'d'});
+    },
   }
 }
 </script>
