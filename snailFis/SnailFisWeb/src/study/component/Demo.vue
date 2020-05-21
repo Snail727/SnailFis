4bf1bae8b649da7a01acdfb9858fee26ee523eef
andrew <snail727@163.com>
2020-05-21 18:33:13 +0800

    组件修改

--- snailFis/SnailFisWeb/src/study/component/Demo.vue
+++ snailFis/SnailFisWeb/src/study/component/Demo.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>1
+  <div>
+    <div><first-component></first-component></div>
+    <div><second-component></second-component></div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+Vue.component('first-component',{template:'<span>第一个组件,全局类型</span>'})//创建全局组件，需要引入vue
+var secondComponent ={template:'<span>第二个组件，局部类型</span>'}
 export default {
+  components:{"second-component":secondComponent},
   data(){
     return{
     }
