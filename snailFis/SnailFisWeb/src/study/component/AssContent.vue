4a3cce04b41cf066b4be75e42c95f493a7ce6590
andrew <snail727@163.com>
2020-05-28 18:19:14 +0800

    过渡

--- snailFis/SnailFisWeb/src/study/component/AssContent.vue
+++ snailFis/SnailFisWeb/src/study/component/AssContent.vue
@@ -2,10 +2,12 @@
   <div>
     <button @click="id=0">Go to demo(组件注册、prop)</button>
     <button @click="id=1">Go to demo(自定义事件)</button>
+    <button @click="id=2">Go to demo(样式过度)</button>
     
     <div style="border:1px solid orange;">
         <demo v-if="id==0"></demo>
         <demo1 v-else-if="id==1"></demo1>
+        <demo2 v-else-if="id==2"></demo2>
     </div>
   </div>
 </template>
@@ -13,8 +15,9 @@
 <script>
 import demo from "@/study/component/Demo.vue";
 import demo1 from "@/study/component/Demo1.vue";
+import demo2 from "@/study/component/Demo2.vue";
 export default {
-    components:{demo,demo1},
+    components:{demo,demo1,demo2},
     data(){
         return{
         id:0
