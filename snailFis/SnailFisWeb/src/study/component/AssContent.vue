8b3f16de22f03110cb0a0760365b565250a44477
andrewew <snail727@163.com>
2020-05-30 20:34:22 +0800

    component

--- snailFis/SnailFisWeb/src/study/component/AssContent.vue
+++ snailFis/SnailFisWeb/src/study/component/AssContent.vue
@@ -1,15 +1,8 @@
 <template>
   <div>
-    <button @click="id=0">Go to demo(组件注册、prop)</button>
-    <button @click="id=1">Go to demo(自定义事件)</button>
-    <button @click="id=2">Go to demo(样式过度)</button>
-    <button @click="id=3">Go to demo(过滤器)</button>
-    
+    <button v-for="tab in tabStrs" :key="tab.id" @click="tabName=tab.id">{{tab.name}}</button>
     <div style="border:1px solid orange;">
-        <demo v-if="id==0"></demo>
-        <demo1 v-else-if="id==1"></demo1>
-        <demo2 v-else-if="id==2"></demo2>
-        <demo3 v-else-if="id==3"></demo3>
+        <keep-alive><component :is="tabName"></component></keep-alive>
     </div>
   </div>
 </template>
@@ -19,14 +12,20 @@ import demo from "@/study/component/Demo.vue";
 import demo1 from "@/study/component/Demo1.vue";
 import demo2 from "@/study/component/Demo2.vue";
 import demo3 from "@/study/component/Demo3.vue";
+import demo4 from "@/study/component/Demo4.vue";
 export default {
-    components:{demo,demo1,demo2,demo3},
-    data(){
-        return{
-        id:0
-        }
-    },
-    methods:{
+  components:{demo,demo1,demo2,demo3,demo4},
+  data(){
+    return{
+      tabStrs:[
+        {id:"demo",name:"组件注册、prop"},
+        {id:"demo1",name:"自定义事件"},
+        {id:"demo2",name:"样式过渡"},
+        {id:"demo3",name:"过滤器"},
+        {id:"demo4",name:"自定义指令和渲染函数"},
+      ],
+      tabName:"demo",
     }
+  }
 }
 </script>
