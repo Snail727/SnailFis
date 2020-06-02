b35fa87fa218fc3ac44f28a075934191e98cb8ed
andrew <snail727@163.com>
2020-06-02 18:36:19 +0800

    ts

--- snailFis/SnailFisWeb/src/study/component/AssContent.vue
+++ snailFis/SnailFisWeb/src/study/component/AssContent.vue
@@ -13,6 +13,7 @@ import demo1 from "@/study/component/Demo1.vue";
 import demo2 from "@/study/component/Demo2.vue";
 import demo3 from "@/study/component/Demo3.vue";
 import demo4 from "@/study/component/Demo4.vue";
+//import demo5 from "@/study/component/Demo5.vue";
 export default {
   components:{demo,demo1,demo2,demo3,demo4},
   data(){
@@ -23,6 +24,7 @@ export default {
         {id:"demo2",name:"样式过渡"},
         {id:"demo3",name:"过滤器"},
         {id:"demo4",name:"自定义指令和渲染函数"},
+        //{id:"demo5",name:"ts"},
       ],
       tabName:"demo",
     }
---
+++ snailFis/SnailFisWeb/src/study/component/Demo5.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="helpMe('sdf')">救我</button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  components: {
+    // TableCom
+  },
+  data() {
+    return {
+      msg:'typescript'
+    };
+  },
+  methods:{
+    helpMe(name:string){
+        alert(this.changeName(name));
+    },
+    changeName(name:string):string{return name+"123"},
+  }
+})
+</script>
